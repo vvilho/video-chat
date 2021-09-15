ae0102a63e861359502fa8ba7b8d767612eefb41
Vilho Voutilainen <vilho.voutilainen@metropolia.fi>
2021-09-15 10:18:11 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/createRoom";
+import Room from "./routes/room";
+import "./App.css";
+
+const App = () => {
+  return (
+      <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route
+              path= "/"
+              exact
+              component={CreateRoom}
+          />
+          <Route
+              path= "/room:roomID"
+              component={Room}
+          />
+        </Switch>
+      </BrowserRouter>
+      </div>
+  );
+}
+export default App;
